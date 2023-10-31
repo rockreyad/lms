7f3ea861583f34a4f4e6f071a50a0cf5db36c635
rockreyad <rockreyad@gmail.com>
2023-10-31 20:55:14 +0600

    responsive all over pages

--- components/common/module-card.tsx
+++ components/common/module-card.tsx
@@ -15,24 +15,24 @@ const MoudleCard: FC<IModuleCard> = ({
   },
 }) => {
   return (
-    <li className="flex first:pt-0 last:pb-0 py-4">
-      <div className="w-1/3">
+    <li className="flex flex-col md:flex-row first:pt-0 last:pb-0 py-4 gap-2 md:gap-0">
+      <div className="w-full md:w-1/3">
         <h3 className="text-lg font-semibold text-cyan-500">{name}</h3>
       </div>
-      <div className="w-2/3">
+      <div className="w-full md:w-2/3">
         <p className="text-gray-800 mb-2">{description}</p>
       </div>
-      <div className="w-1/3 text-end">
-        <p className="text-gray-600 text-sm mb-2">
+      <div className="w-full md:w-1/3 text-end flex md:block divide-x md:divide-x-0 divide-gray-300">
+        <p className="text-gray-600 text-sm mb-2 px-1">
           Lessons: <span className="font-medium">{lessonsDuration}</span>
         </p>
-        <p className="text-gray-600 text-sm mb-2">
+        <p className="text-gray-600 text-sm mb-2 px-1">
           Projects: <span className="font-medium">{projectsDuration}</span>
         </p>
-        <p className="text-gray-600 text-sm mb-2">
+        <p className="text-gray-600 text-sm mb-2 px-1">
           Quizzes: <span className="font-medium">{quizzesDuration}</span>
         </p>
-        <p className="text-gray-600 text-sm mb-2">
+        <p className="text-gray-600 text-sm mb-2 px-1">
           Total: <span className="font-medium">{totalDuration}</span>
         </p>
       </div>
--- components/course-details/course-modules.tsx
+++ components/course-details/course-modules.tsx
@@ -7,7 +7,10 @@ interface ICourseModules {
 }
 const CourseModules: FC<ICourseModules> = ({ modules }) => {
   return (
-    <ul role="list" className="grid mx-10 gap-4 p-6 divide-y divide-slate-200">
+    <ul
+      role="list"
+      className="grid gap-4 divide-y divide-slate-200 px-6 py-24 lg:px-10"
+    >
       {modules.map((module, _idx) => (
         <MoudleCard key={module.id + _idx} module={module} />
       ))}
