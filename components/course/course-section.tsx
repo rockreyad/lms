ef9f6bb458c8e9f0c55ed6b958d5aa322d960192
rockreyad <rockreyad@gmail.com>
2023-10-31 15:43:24 +0600

    chore: category filter

--- components/course/course-section.tsx
+++ components/course/course-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Course } from "@/types/course.schema";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, memo } from "react";
 import CourseCard from "../course-card";
 import SearchCourse from "./search-course";
 import { useSearchParams } from "next/navigation";
@@ -8,14 +8,25 @@ import { useSearchParams } from "next/navigation";
 interface ICourseSection {
   courses: Course[];
 }
-const CourseSection: FunctionComponent<ICourseSection> = ({ courses }) => {
+const CourseSection: FunctionComponent<ICourseSection> = memo(({ courses }) => {
   const searchParams = useSearchParams();
   let search = searchParams.get("search");
+  let category = searchParams.get("category");
+
+  const filteredCourses = courses.filter((course) => {
+    const courseNameMatchesSearch =
+      !search || course.name.toLowerCase().includes(search.toLowerCase());
+
+    const courseCategoryMatchesFilter =
+      !category || (course.category && course.category.name === category);
+
+    return courseNameMatchesSearch && courseCategoryMatchesFilter;
+  });
   return (
     <div className="space-y-10">
       <SearchCourse />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {courses
+        {filteredCourses
           .filter((course) => {
             if (!search) return true;
             return course.name.toLowerCase().includes(search.toLowerCase());
@@ -26,6 +37,7 @@ const CourseSection: FunctionComponent<ICourseSection> = ({ courses }) => {
       </div>
     </div>
   );
-};
+});
 
 export default CourseSection;
+CourseSection.displayName = "CourseSection";
