507c889e180309079b99a9e76c5dbbb3273d1dab
rockreyad <rockreyad@gmail.com>
2023-11-02 07:32:55 +0600

    style: video section on hero area and stats place change

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -4,17 +4,17 @@ import React from "react";
 
 export const HeroSection = async () => {
   return (
-    <div className="relative bg-slate-800">
+    <div className="relative bg-slate-800 flex flex-col lg:flex-row">
       <div className="mx-auto max-w-7xl">
         <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
-          <svg
+          {/* <svg
             className="absolute inset-y-0 right-0 hidden h-full w-80 translate-x-1/2 transform fill-slate-800 lg:block"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
             aria-hidden="true"
           >
             <polygon points="0,0 90,0 50,100 0,100" />
-          </svg>
+          </svg> */}
 
           <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0 bg-slate-800">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
@@ -56,7 +56,12 @@ export const HeroSection = async () => {
           </div>
         </div>
       </div>
-      <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <iframe
+        className="aspect-video w-full lg:w-1/2 h-fit m-auto rounded-lg shadow-lg shadow-cyan-800/20"
+        src="https://www.youtube.com/embed/a4qymb4c57g?si=TJUjsvqP8m9qXEo-"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
+      {/* <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <Image
           className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
           src="/images/hero.jpeg"
@@ -64,7 +69,7 @@ export const HeroSection = async () => {
           width={1920}
           height={1280}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
