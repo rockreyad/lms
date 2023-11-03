2ce4760a5adecb258b0a72ce4c771e6ebb57af34
maruffahmed <marufah76@gmail.com>
2023-11-03 16:18:11 +0600

    ConvertKit submission is complete

---
+++ components/common/action_banner.tsx
@@ -0,0 +1,46 @@
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/20/solid";
+import { z } from "zod";
+
+export function SuccessBanner({ message }: { message: string }) {
+  return (
+    <div className="rounded-md bg-green-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-green-800">{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function ErrorBanner({ errors }: { errors: z.ZodIssue[] | null }) {
+  const totalErrors = errors?.length ? errors.length : 0;
+  return (
+    <div className="rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-red-800">
+            There were {totalErrors} errors with your submission
+          </h3>
+          <div className="mt-2 text-sm text-red-700">
+            <ul role="list" className="list-disc space-y-1 pl-5">
+              {errors?.length &&
+                errors.map((error, index) => (
+                  <li key={index}>{error.message}</li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ module/actions/contact_form.action.ts
@@ -0,0 +1,71 @@
+"use server";
+import { z } from "zod";
+
+const convertKitFormId = process.env.CONVERTKIT_FORM_ID;
+const convertKitApiKey = process.env.CONVERTKIT_API_KEY;
+
+const convertKitFormSchema = z.object({
+  email: z.string().email(),
+  full_name: z.string().min(3, {
+    message: "Please enter your full name.",
+  }),
+  message: z.string().min(1, {
+    message: "Please enter your message.",
+  }),
+});
+
+export async function subscribeConvertKit(prevState: any, formData: FormData) {
+  try {
+    const email = formData.get("email");
+    const full_name = formData.get("full_name");
+    const message = formData.get("message");
+
+    const parseData = convertKitFormSchema.parse({
+      email,
+      full_name,
+      message,
+    });
+    const body = JSON.stringify({
+      api_key: convertKitApiKey,
+      email: parseData.email,
+      fields: {
+        full_name: parseData.full_name,
+        message: parseData.message,
+      },
+    });
+    const response = await fetch(
+      `https://api.convertkit.com/v3/forms/${convertKitFormId}/subscribe`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body,
+      }
+    );
+    const data = await response.json();
+    if (data.error) {
+      return {
+        error: true,
+        message: data.error.message,
+        errors: null,
+      };
+    }
+    return {
+      error: false,
+      message:
+        "You're in! Thank you for subscribing. Please confirm it in your email.",
+      errors: null,
+    };
+  } catch (e) {
+    console.error(e);
+    if (e instanceof z.ZodError) {
+      return { error: true, message: null, errors: e.errors };
+    }
+    return {
+      error: true,
+      message:
+        "Apologies, an error occurred. Failed to subscribe, please try again.",
+    };
+  }
+}
