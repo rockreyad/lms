54d5c615fe8fac53e679484cbd8ed5fecfc64b00
rockreyad <rockreyad@gmail.com>
2023-11-11 14:21:46 +0600

    color changed

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -48,13 +48,13 @@ const tiers = [
 ];
 const PlansPage = () => {
   return (
-    <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="relative isolate bg-slate-900 px-6 py-24 sm:py-32 lg:px-8">
       <div
         className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
         aria-hidden="true"
       >
         <div
-          className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="mx-auto aspect-[1155/678] w-[72.1875rem] "
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -65,11 +65,11 @@ const PlansPage = () => {
         <h2 className="text-base font-semibold leading-7 text-indigo-600">
           Pricing
         </h2>
-        <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <p className="mt-2 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl">
           The right price for you, whoever you are
         </p>
       </div>
-      <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+      <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
         Your child will learn to code with fun, interactive games and
         activities. They will learn the fundamentals of coding, develop
         problem-solving skills, and build creative projects.
